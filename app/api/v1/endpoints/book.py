d447dac62651ccd59605e92b3bf292eb07accee5
AdemolaPersonal <ademola.ari@gmail.com>
2023-12-08 09:20:07 -0600

    endpoints

---
+++ app/api/v1/endpoints/book.py
@@ -0,0 +1,30 @@
+import json
+from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
+from app.database.mock import MockBooks
+from app.model.book import Book, BookInDB
+from app.service import csv
+from typing import List
+
+router = APIRouter(
+    tags=['books'],
+)
+
+
+@router.get("/books")
+def get_books():
+    return {'books': MockBooks}
+
+
+@router.get("/books/{book_id}")
+def get_book(book_id: int):
+    book = next((book for book in MockBooks if book.id == book_id), None)
+    return {'book': book}
+
+
+@router.post("/books")
+def create_book(book: Book, status_code=status.HTTP_201_CREATED) -> List[BookInDB]:
+    MockBooks.append(book)
+    print(MockBooks)
+
+    return MockBooks
