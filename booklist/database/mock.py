c83b3590592139022e924da28d8309c3e6621df0
AdemolaPersonal <ademola.ari@gmail.com>
2023-12-08 09:26:55 -0600

    rename root app

--- booklist/database/mock.py
+++
@@ -1,7 +0,0 @@
-from booklist.model.book import BookInDB
-
-MockBooks = [
-    BookInDB(id=1, title="Northanger Abbey", author="Jane Austen", price="18.20", inventory=3),
-    BookInDB(id=2, title="Siddhartha", author="Hermann Hesse", price="6.50", inventory=2),
-    BookInDB(id=3, title="The Trial", author="Franz Kafka", price="9.20", inventory=1),
-]
